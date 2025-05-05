2a113ba874b3e6c65d7103079d7abec0def73537
unknown <amargajula123@gmail.com>
2025-05-05 22:54:03 +0530

    app.py,example.ibynb and housing/exception/__init__.py modifeid

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from housing.logger import logging
+import sys
+from housing.exception import HousingException
 
 #deploy
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("we are testing logging module")
-    return "Practicing ML Project again"
+    try:
+        raise Exception("we are testing our custome Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+
+    return "Practicing ML Project again "
 
 if __name__=="__main__":
     app.run(debug=True)
