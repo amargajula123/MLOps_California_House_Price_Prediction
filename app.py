388cf89cb7348f9a70045ba62612a56e5b1a53c9
unknown <amargajula123@gmail.com>
2025-05-05 19:26:37 +0530

    app.py example  ousing/logger/__init__.py modified

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from housing.logger import logging
 
 #deploy
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("we are testing logging module")
     return "Practicing ML Project again"
 
 if __name__=="__main__":
